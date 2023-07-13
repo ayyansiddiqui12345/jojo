c5d09664f08bc6afc116036ab94d98bae7bc0b25
ayyansiddiqui12345 <ayyanbhaai476@gmail.com>
2023-07-13 15:44:40 +0500

    changes

--- ayyan.js
+++ ayyan.js
@@ -1,28 +1,6 @@
-//git add .
-//git commit -m "xyz"
-class railwayForm{
-    submit(){
-        alert(this.name + " your form is submitted")
-    }   
-
-    cancel(){
-        alert(this.name + " your form is cancelled")
-    }
-    fill(givenname){
-        this.name = givenname
-
-    }
-}
-
-let ayyan = new railwayForm()
-ayyan.fill("ayyan")
-
-let anas = new railwayForm()
-anas.fill("anas")
-
-
-
-ayyan.submit()
-anas.submit()
-ayyan.cancel()
-
+    // // //git add .
+    // // //git commit -m "xyz"
+    
+    // fcc functional programming
+    
+    
